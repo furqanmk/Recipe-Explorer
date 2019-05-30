ca7137cef8d9934749c5cca257fd4f8da31bedeb
Furqan Khan <furqan@freeletics.com>
2019-05-30 15:40:28 +0200

    Implemented Recipe Details screen

---
+++ Recipe Explorer/Spaces/RecipeDetails/RecipeDetailsViewController.swift.swift
@@ -0,0 +1,63 @@
+//
+//  RecipeDetailsViewController.swift
+//  Recipe Explorer
+//
+//  Created by Furqan on 2019-05-30.
+//  Copyright © 2019 Furqan. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+
+class RecipeDetailsViewController: UIViewController {
+    
+    @IBOutlet private weak var recipeImageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var descriptionLabel: UILabel!
+    @IBOutlet private weak var chefNameLabel: UILabel!
+    @IBOutlet private weak var tagsLabel: UILabel!
+    
+    private let viewModel: RecipeDetailsViewModel
+    
+    /// Initalizes view controller with an instance of view model.
+    ///
+    /// - Parameter viewModel: Instance of RecipeDetailsViewModel.
+    init(viewModel: RecipeDetailsViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Recipe Details"
+        setupUI()
+    }
+
+    /// Assigns values to UI elements from the view model.
+    private func setupUI() {
+        titleLabel.text = viewModel.title
+        descriptionLabel.text = viewModel.description
+        chefNameLabel.text = viewModel.chefName
+        tagsLabel.text = viewModel.tags.joined(separator: ", ")
+        
+        recipeImageView.image = #imageLiteral(resourceName: "food_placeholder")
+        if let imageUrl = viewModel.imageUrl {
+            recipeImageView.sd_setImage(with: imageUrl)
+        }
+        roundifyProfileImageView()
+    }
+    
+    /// Makes the shape of the profile image view circular.
+    private func roundifyProfileImageView() {
+        let dimension = min(recipeImageView.frame.width, recipeImageView.frame.height)
+        recipeImageView.layer.cornerRadius = dimension / 2
+        recipeImageView.layer.masksToBounds = true
+    }
+    
+}
---
+++ Recipe Explorer/Spaces/RecipeDetails/RecipeDetailsViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  RecipeDetailsViewModel.swift
+//  Recipe Explorer
+//
+//  Created by Furqan on 2019-05-30.
+//  Copyright © 2019 Furqan. All rights reserved.
+//
+
+import Foundation
+
+final class RecipeDetailsViewModel {
+    
+    // - MARK: Private properties
+    
+    private let recipe: Recipe
+    
+    // - MARK: Initializers
+    
+    /// Initializes view model for RecipeDetailsViewController
+    ///
+    /// - Parameter Recipe: Instance of a Recipe object.
+    init(with recipe: Recipe) {
+        self.recipe = recipe
+    }
+    
+    // - MARK: Public properties
+    
+    var title: String {
+        return recipe.title
+    }
+    
+    var description: String {
+        return recipe.description
+    }
+    
+    var chefName: String? {
+        return recipe.chefName
+    }
+    
+    var tags: [String] {
+        return recipe.tags
+    }
+    
+    var imageUrl: URL? {        
+        guard
+            let imageUrlString = recipe.imageUrl,
+            let url = URL(string: imageUrlString) else {
+                return nil
+        }
+        return url
+    }
+    
+}
